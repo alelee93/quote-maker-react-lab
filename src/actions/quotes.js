854e17d0be4b8352152388726755fc2cb0e0d193
alelee93 <leevargasale@gmail.com>
2021-03-02 20:06:27 -0600

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,39 @@
 // TODO: Create action creators as defined in tests
+export const ADD_QUOTE = 'ADD_QUOTE'
+export const REMOVE_QUOTE = 'REMOVE_QUOTE'
+export const UPVOTE_QUOTE = 'UPVOTE_QUOTE'
+export const DOWNVOTE_QUOTE = 'DOWNVOTE_QUOTE'
+import uuid from 'uuid'
+
+export const addQuote = (quote) => {
+    let id = uuid();
+    return{
+        type: ADD_QUOTE, //type should always be a string
+        quote: {
+            id,
+            ...quote,
+            votes: 0
+        }  //equivalent to quote: quote
+    }
+}
+
+export const removeQuote = (quoteId) => {
+    return{
+        type: REMOVE_QUOTE, //type should always be a string
+        quoteId  //equivalent to quoteId: quoteId
+    }
+}
+
+export const upvoteQuote = (quoteId) => {
+    return{
+        type: UPVOTE_QUOTE, //type should always be a string
+        quoteId  //equivalent to quoteId: quoteId
+    }
+}
+
+export const downvoteQuote = (quoteId) => {
+    return{
+        type: DOWNVOTE_QUOTE, //type should always be a string
+        quoteId  //equivalent to quoteId: quoteId
+    }
+}
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,35 @@
+import {ADD_QUOTE, REMOVE_QUOTE, UPVOTE_QUOTE, DOWNVOTE_QUOTE} from '../actions/quotes'
+
 export default (state = [], action) => {
-  return state;
+  switch (action.type){
+    case ADD_QUOTE:
+      return [...state, action.quote]
+      //return state.concat(action.quote)
+    case REMOVE_QUOTE:
+      return state.filter(quote => quote.id !== action.quoteId) //return quotes that dont have action.id 
+    case UPVOTE_QUOTE:
+      return state.map(quote => {
+        if(quote.id === action.quoteId){
+          return{
+            ...quote,
+            votes: quote.votes + 1
+          }
+        } else {
+          return quote
+        }
+      })
+    case DOWNVOTE_QUOTE:
+      return state.map(quote => {
+        if(quote.id === action.quoteId){
+          return{
+            ...quote,
+            votes: quote.votes === 0 ? 0 : quote.votes - 1
+          }
+        } else {
+          return quote
+        }
+      })
+    default:
+      return state
+  }
 }
